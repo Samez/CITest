8d737497d6a8374be81177efae251227183987ad
Samez <Samez@MacBook-Air-Oleg.local>
2013-11-02 05:58:33 +0400

    project created

---
+++ CITest/CITest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CITest
+//
+//  Created by Oleg Sobolev on 02.11.13.
+//  Copyright (c) 2013 gg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
